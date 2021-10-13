c874d616cb059737098d7e5de014f1932cad5eea
Tfloow <88899420+Tfloow@users.noreply.github.com>
2021-10-13 16:44:59 +0200

    first test not perfect and i don't know what i'm even supposed to do

---
+++ main.py
@@ -0,0 +1,25 @@
+f = open("test.txt", "a")
+f.write("test \n")
+f.close()
+print(open("test.txt", "r").read())
+x = 1
+a = 0.0134
+
+
+def delete(file):
+    d = open(file, "w")
+    d.write("")
+    d.close()
+
+
+def angle(function):
+    tangent = []
+    for x in range(51):
+        derivated = 2*a*x
+        print(derivated)
+        app = str(str(derivated) + "(x-" + str(x//2) + ")")
+        tangent.append(app)
+    print(tangent)
+
+
+angle(a*x**2)
