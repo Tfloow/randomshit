d7d26a5f8dc74fe42e5c12744e50da433a4aa30b
Tfloow <88899420+Tfloow@users.noreply.github.com>
2021-10-17 19:17:35 +0200

    Fixed an issue with k. Now it's correctly computed and the Sacceleration function is reliable

--- main.py
+++ main.py
@@ -7,21 +7,38 @@ def delete(file):
     d.close()
 
 
-def angle():
+def angle(x1, x2):
     tangent = []
-    for x in range(105):
-        derivated = 2 * 35/2601 * x -70/51
+    for x in range(x1, x2):
+        derivated =70/2601 * x -70/51
         d = 35/2601*x**2-70/51*x+40 - derivated*x
         angleLow = numpy.arctan(numpy.absolute(derivated))*180/numpy.pi
         angleUp = 90 - angleLow
         app = str(str(x)+ str(" ") + str(derivated) + "(x)+" + str(d)+ " upper angle: "+ str(angleUp) + " lower angle :" + str(angleLow)+str("\n"))
         tangent.append(app)
+        powDer = pow(derivated, 2)
+        powDer1 = powDer+1
+        powRac = pow(powDer1, 3)
+        powRac = pow(powRac, 1/2)
+        l=None
+        ktop = 70/2601
+        k = ktop/powRac
     print(numpy.array(tangent))
     pretty = "example: "
-    for f in range(105):
+    """for f in range(x1, x2):
         pretty+= tangent[f]
-    open("modified data.txt", "w").write(pretty)
+    open("modified data.txt", "a").write(pretty)
+    """
+    return angleLow, k
 
+def Sacceleration(h,e, Sspeed, r):
+    conAngle =angle(73,74)[0]*numpy.pi/180
+    print(conAngle)
+    k = angle(73,74)[1]
+    print(k)
+    g = 9.81
+    Sacceleration = 5*h*(-e*Sspeed**2*k-g*numpy.cos(conAngle)*e*Sspeed+h*g*numpy.sin(conAngle))/2*r**2+5*h**2
+    print(Sacceleration)
 
 def speed(place):
     t = []
@@ -49,7 +66,9 @@ def speed(place):
     print(numpy.array(resultx))
     print(numpy.array(resulty))
 
-delete("modified data.txt")
-angle()
-print(open("billeDataset2.txt", "r").readlines()[3])
-speed(20)
+#delete("modified data.txt")
+#angle()
+#print(open("billeDataset2.txt", "r").readlines()[3])
+#speed(20)
+
+Sacceleration(0.00795, 0.1,10, 0.008)
